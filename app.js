291b4a357928823f487607f1daf5f34e12077233
“stevengo497” <ngo.steve497@yahoo.com>
2018-04-16 08:27:30 -0600

    new route

---
+++ app.js
@@ -0,0 +1,38 @@
+/* dependencies & app setup */
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const app = express();
+const pizza = require('./db/pizza.js');
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
+});
+
+/* set the view engine */
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+/* error logger, static routes */
+app.use(logger('dev'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public')) // added in order to read public files
+
+app.get('/pizza', (req, res) => {
+	console.log(pizza) // logs in terminal
+	const pizzaString = JSON.stringify(pizza);
+	res.render("index", {pizzaString});
+});
+
+app.get('/pizza/:id', (req, res) => {
+	let id = req.parmas.id
+	const pizzaString = JSON.stringify(pizza);
+	res.render("index", {pizzaString});
+});
+
+/* error handler */
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Oops! Not found.'});
+});
